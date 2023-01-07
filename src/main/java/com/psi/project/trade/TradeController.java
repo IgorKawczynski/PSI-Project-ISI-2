6f7b8fa2a9ad109214623f9abf114418ff6bcda0
Adam <trentowski.adam@gmail.com>
2023-01-07 16:13:11 +0100

    Added trade controller

--- src/main/java/com/psi/project/trade/TradeController.java
+++ src/main/java/com/psi/project/trade/TradeController.java
@@ -1,4 +1,50 @@
 package com.psi.project.trade;
 
+import com.psi.project.trade.dtos.TradeRequestDTO;
+import com.psi.project.trade.dtos.TradeResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/trade")
 public class TradeController {
+
+    private final TradeService tradeService;
+
+    public TradeController(TradeService tradeService) {
+        this.tradeService = tradeService;
+    }
+
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<TradeResponseDTO> getTrades() {
+        return tradeService.getTrades();
+    }
+
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.FOUND)
+    public TradeResponseDTO getTradeById(@RequestParam("id") Long id) {
+        return tradeService.getTradeById(id);
+    }
+
+    @PostMapping(path = "", produces = "application/json", consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addTrade(@RequestBody TradeRequestDTO tradeRequestDTO) {
+        tradeService.addTrade(tradeRequestDTO);
+    }
+
+    @PatchMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public String updateTradeById(@PathVariable Long id, @RequestBody Double value) {
+        return tradeService.updateTradeById(id, value);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String deleteTradeById (@PathVariable Long id) {
+        return tradeService.deleteTrade(id);
+    }
+
 }

303d527f8fcc9b8153dfd42780462258f325a5dc
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-01-22 12:31:16 +0100

    Added crud methods to item graphql service

--- src/main/java/com/psi/project/graphql/item/ItemGRAPHQLService.java
+++ src/main/java/com/psi/project/graphql/item/ItemGRAPHQLService.java
@@ -1,4 +1,37 @@
 package com.psi.project.graphql.item;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ItemGRAPHQLService {
+
+    @Autowired
+    private ItemGRAPHQLRepository itemGRAPHQLRepository;
+
+    public List<ItemGRAPHQL> getAllItems() {
+        return itemGRAPHQLRepository.findAll();
+    }
+
+    public ItemGRAPHQL getItemById(Integer id) {
+        return itemGRAPHQLRepository.findItemGRAPHQLById(id);
+    }
+
+    public ItemGRAPHQL addItem(ItemGRAPHQL item) {
+        return itemGRAPHQLRepository.save(item);
+    }
+
+    public ItemGRAPHQL updateItemById(Integer id, String name, Integer cost) {
+        var item = itemGRAPHQLRepository.findItemGRAPHQLById(id);
+        item.setName(name);
+        item.setCost(cost);
+        return item;
+    }
+
+    public Integer deleteItemById(Integer id) {
+        itemGRAPHQLRepository.deleteById(id);
+        return id;
+    }
 }

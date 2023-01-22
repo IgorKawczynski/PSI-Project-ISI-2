5f2ce3e1c7228fdf865880df851dd0e90abd99fa
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-01-22 12:30:55 +0100

    Added crud methods to user graphql service

--- src/main/java/com/psi/project/graphql/user/UserGRAPHQLService.java
+++ src/main/java/com/psi/project/graphql/user/UserGRAPHQLService.java
@@ -1,4 +1,39 @@
 package com.psi.project.graphql.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserGRAPHQLService {
+
+    @Autowired
+    private UserGRAPHQLRepository userGRAPHQLRepository;
+
+    public List<UserGRAPHQL> getAllUsers() {
+        return userGRAPHQLRepository.findAll();
+    }
+
+    public UserGRAPHQL getUserById(Integer id) {
+        return userGRAPHQLRepository.findUserGRAPHQLById(id);
+    }
+
+    public UserGRAPHQL addUser(UserGRAPHQL user) {
+        return userGRAPHQLRepository.save(user);
+    }
+
+    public UserGRAPHQL updateUserById(Integer id, String email, String firstName, String lastName, String pesel) {
+        var user = userGRAPHQLRepository.findUserGRAPHQLById(id);
+        user.setEmail(email);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setPesel(pesel);
+        return user;
+    }
+
+    public Integer deleteUserById(Integer id) {
+        userGRAPHQLRepository.deleteById(id);
+        return id;
+    }
 }

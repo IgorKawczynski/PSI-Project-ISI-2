072ba4a895091d8b2310c17bda6280376dffad4c
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-01-22 12:31:07 +0100

    Added crud methods to trade graphql service

--- src/main/java/com/psi/project/graphql/trade/TradeGRAPHQLService.java
+++ src/main/java/com/psi/project/graphql/trade/TradeGRAPHQLService.java
@@ -1,4 +1,38 @@
 package com.psi.project.graphql.trade;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TradeGRAPHQLService {
+
+    @Autowired
+    private TradeGRAPHQLRepository tradeGRAPHQLRepository;
+
+    public List<TradeGRAPHQL> getAllTrades() {
+        return tradeGRAPHQLRepository.findAll();
+    }
+
+    public TradeGRAPHQL getTradeById(Integer id) {
+        return tradeGRAPHQLRepository.findTradeGRAPHQLById(id);
+    }
+
+    public TradeGRAPHQL addTrade(TradeGRAPHQL trade) {
+        return tradeGRAPHQLRepository.save(trade);
+    }
+
+    public TradeGRAPHQL updateTradeById(Integer id, Integer value, Integer buyerId, Integer sellerId) {
+        var trade = tradeGRAPHQLRepository.findTradeGRAPHQLById(id);
+        trade.setValue(value);
+        trade.setBuyerId(buyerId);
+        trade.setSellerId(sellerId);
+        return trade;
+    }
+
+    public Integer deleteTradeById(Integer id) {
+        tradeGRAPHQLRepository.deleteById(id);
+        return id;
+    }
 }

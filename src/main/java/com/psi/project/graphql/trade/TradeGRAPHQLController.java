7152fa1a8edde7440b6fe647db4608ca825a2e97
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-01-22 12:32:11 +0100

    Updated trade graphql controller with crud methods linked with database

--- src/main/java/com/psi/project/graphql/trade/TradeGRAPHQLController.java
+++ src/main/java/com/psi/project/graphql/trade/TradeGRAPHQLController.java
@@ -2,7 +2,7 @@ package com.psi.project.graphql.trade;
 
 import com.psi.project.graphql.item.ItemGRAPHQL;
 import com.psi.project.graphql.user.UserGRAPHQL;
-import io.swagger.models.auth.In;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -14,19 +14,20 @@ import java.util.List;
 @Controller
 public class TradeGRAPHQLController {
 
-    @QueryMapping
-    public TradeGRAPHQL tradeById(@Argument Integer id) {
-        return TradeGRAPHQL.getById(id);
-    }
+    @Autowired
+    private TradeGRAPHQLService tradeGRAPHQLService;
 
+    /**
+     Metody Korzystające tylko z modelu (nie połączone z bazą danych)
+     */
     @QueryMapping
     public List<TradeGRAPHQL> allTrades() {
         return TradeGRAPHQL.getAllTrades();
     }
 
-    @MutationMapping
-    public TradeGRAPHQL updateTradeById(@Argument Integer id, @Argument Integer value) {
-        return TradeGRAPHQL.updateById(id, value);
+    @QueryMapping
+    public TradeGRAPHQL tradeById(@Argument Integer id) {
+        return TradeGRAPHQL.getById(id);
     }
 
     @QueryMapping
@@ -42,6 +43,34 @@ public class TradeGRAPHQLController {
     @QueryMapping
     public TradeGRAPHQL tradeBySeller(@Argument Integer sellerId) {
         return TradeGRAPHQL.getBySeller(sellerId);
+    }
+
+    /**
+     Metody Korzystające z serwisu - połączone z bazą danych
+     */
+    @QueryMapping
+    public List<TradeGRAPHQL> getAllTrades() {
+        return tradeGRAPHQLService.getAllTrades();
+    }
+
+    @QueryMapping
+    public TradeGRAPHQL getTradeById(@Argument Integer id) {
+        return tradeGRAPHQLService.getTradeById(id);
+    }
+
+    @MutationMapping
+    public TradeGRAPHQL addTrade(@Argument TradeGRAPHQL trade) {
+        return tradeGRAPHQLService.addTrade(trade);
+    }
+
+    @MutationMapping
+    public TradeGRAPHQL updateTradeById(@Argument Integer id, @Argument Integer value, @Argument Integer buyerId, @Argument Integer sellerId) {
+        return tradeGRAPHQLService.updateTradeById(id, value, buyerId, sellerId);
+    }
+
+    @MutationMapping
+    public Integer deleteTradeById(@Argument Integer id) {
+        return tradeGRAPHQLService.deleteTradeById(id);
     }
 
     @SchemaMapping
